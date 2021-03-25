da96e32e880ec9418f3337b99e6522e73f946718
Evyn Newton <newton.evyn@gmail.com>
2021-03-25 11:54:43 -0400

    removed before actions

--- app/controllers/alliances_controller.rb
+++ app/controllers/alliances_controller.rb
@@ -1,6 +1,6 @@
 class AlliancesController < ApplicationController
   before_action :set_alliance, only: [:show, :update, :destroy]
-  before_action :authorize_request
+  before_action :authorize_request, only: [:create, :update, :destroy]
 
   # GET /alliances
   def index
